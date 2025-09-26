949d0f234a3671eedaa15329b630357e7e9fb84e
Shravani Kadu <KSHRAX21@GMAIL.COM>
2025-09-26 19:11:51 +0530

    Delete src directory

--- src/api/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: 'https://api.example.com', // Replace with your API base URL
-  timeout: 10000,
-})
-
-export default instance
